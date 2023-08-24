c8eb6a61c027785dd0f899565348450025f01a9f
Ytalo Mateus <99446046+YtaloSantoss@users.noreply.github.com>
2023-08-24 18:02:00 -0300

    atlz

--- 5_FORM_EM_REACT/form/src/components/MyForm.js
+++ 5_FORM_EM_REACT/form/src/components/MyForm.js
@@ -1,20 +1,33 @@
 import './MyForm.css'
 import { useState } from 'react';
 
-const MyForm = () => {
+const MyForm = ({ user }) => {
   // 3- gerenciamento de dados 
-  const [name, setName] = useState('')
-  const [email, setEmail] = useState('')
+  const [name, setName] = useState(user ? user.name : "")
+  const [email, setEmail] = useState(user ? user.email : "")
+  const [bio, setBio] = useState(user ? user.bio : "")
 
   const handleName = (e) => {
     setName(e.target.value)
   }
-  console.log(name)
-  console.log(email)
+
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+    console.log(name)
+    console.log(email)
+    console.log(bio)
+
+    setName("")
+    setEmail("")
+    setBio("")
+  }
+
   return (
     <div>
       {/*1 - criacao de form */}
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="name">Nome: </label>
           <input
@@ -22,6 +35,7 @@ const MyForm = () => {
             name="name"
             placeholder="Digite seu nome"
             onChange={handleName}
+            value={name}
           />
         </div>
         <label >
@@ -31,8 +45,19 @@ const MyForm = () => {
             name="email"
             placeholder="Digite o seu e-mail"
             onChange={(e) => setEmail(e.target.value)}
+            value={email}
           />
         </label>
+        <label>
+          <span >Bio:</span>
+          <textarea
+            name="bio"
+            placeholder="Discrição do usuário"
+            onChange={(e) => setBio(e.target.value)}
+            value={bio}
+          ></textarea>
+        </label>
+
         <input
           type="submit"
           value="Enviar"
